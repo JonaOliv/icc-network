177b0d73b9710ce8cf7488e964e73ded1a2f3e84
Jonathan Oliva <rutenio441@hotmail.com>
2015-07-16 17:29:00 -0600

    Companeros revisenlo

--- routes/proyectos/DocumentoSubir.js
+++ routes/proyectos/DocumentoSubir.js
@@ -1,38 +1,50 @@
 function DocumentoSubir(req,res){
-  //192.168.29.145:2424/usuarios/testDocumentos1
-  //http://192.168.29.145:2424/usuarios/testDocumentos1
   var fs = require('fs');
   var mkdirp = require('mkdirp');
 
-  var strDireccionCarpArchivos = '/home/jonathan/icc-network/archivos/';
-  var strProyecto = 'IDProyecto/';
-  var strNombreArchivo = req.files.archivo.name;
+  if(req.method=='POST'){
+    var strDireccionCarpArchivos = '/home/jonathan/icc-network/archivos/';
+    var strProyecto = req.params.ProyectoID;
+    var strNombreArchivo = req.files.archivo.name;
+    var boolNingunError = true;
 
-  //crea directorio si no existe
-  mkdirp(strDireccionCarpArchivos + strProyecto, function(err) {
+    //crea directorio si no existe
+    mkdirp(strDireccionCarpArchivos + strProyecto, function(err) {
 
-  });
-
-  //Leer posicion actual del archivo y escribbir archivo en el destino "dest"
-  var source = fs.createReadStream(req.files.archivo.path);
-  var dest = fs.createWriteStream(strDireccionCarpArchivos + strProyecto + strNombreArchivo);
-
-  source.pipe(dest);
-  source.on('end', function() {
-    //borrar archivo original
-    fs.unlink(req.files.archivo.path,function(err) {
-      if(err)
-        console.log("Error borrando archivo");
     });
-  });
-  source.on('error', function(err) { });
-/*
-  console.log(req.files.archivo.name);
-  console.log(req.files.archivo.path);
-  console.log(__dirname);*/
 
-  console.log(req.files.archivo);
-  res.send(req.files.archivo);
+    //Leer posicion actual del archivo y escribir archivo en el destino "dest"
+    var source = fs.createReadStream(req.files.archivo.path);
+    var dest = fs.createWriteStream(strDireccionCarpArchivos + strProyecto + strNombreArchivo);
+
+    source.pipe(dest);
+
+    source.on('end', function() {
+      //borrar archivo original
+      fs.unlink(req.files.archivo.path,function(err) {
+        if(err){
+          console.log("Error borrando archivo");
+        }
+      });
+    });
+
+    source.on('error', function(err) {
+      boolNingunError=false;
+      error(2003,"",500,req,res);
+    });
+
+    if(boolNingunError){
+      //Espacio para lo relacionado con la base de datos
+    }
+
+
+    console.log(req.files.archivo);
+    res.send(req.files.archivo);
+  }
+  
+  if(req.method == 'GET'){
+    //res.send("Hello World!");
+  }
 }
 
 
